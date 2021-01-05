c705f16197d75da1d32178b8261223092bfcf277
chankung9 <59549960+chankung9@users.noreply.github.com>
2021-01-05 22:15:57 +0700

    Added GraphQl

--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class LoggingInterceprot implements NestInterceptor {
@@ -14,22 +15,37 @@ export class LoggingInterceprot implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    console.log('Before...');
-
     const req = context.switchToHttp().getRequest();
-    const method = req.method;
-    const url = req.url;
     const now = Date.now();
+    if (req) {
+      const method = req.method;
+      const url = req.url;
 
-    return next
-      .handle()
-      .pipe(
-        tap(() =>
-          Logger.log(
-            `${method} ${url} ${Date.now() - now}ms`,
-            context.getClass().name,
+      return next
+        .handle()
+        .pipe(
+          tap(() =>
+            Logger.log(
+              `${method} ${url} ${Date.now() - now}ms`,
+              context.getClass().name,
+            ),
           ),
-        ),
-      );
+        );
+    } else {
+      const ctx: any = GqlExecutionContext.create(context);
+      const resolverName = ctx.constructorRef.name;
+      const info = ctx.getInfo();
+
+      return next
+        .handle()
+        .pipe(
+          tap(() =>
+            Logger.log(
+              `${info.parentType} "${info.fieldName}" ${Date.now() - now}ms`,
+              resolverName,
+            ),
+          ),
+        );
+    }
   }
 }
